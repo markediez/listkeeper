98b3d54904350f5958a07750d5f00717836cc0df
Mark Diez <msdiez@ucdavis.edu>
2016-09-20 10:47:25 -0700

    Add method to get current date in yyyy-mm-dd hh:mm:ss.sss format

--- app/src/main/java/me/markediez/listkeeper/Item.java
+++ app/src/main/java/me/markediez/listkeeper/Item.java
@@ -1,5 +1,9 @@
 package me.markediez.listkeeper;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Mark Diez on 9/20/2016.
  */
@@ -9,5 +13,17 @@ public class Item {
     public String createdAt;
     public String updatedAt;
 
-    // TODO: Constructor should have default values for createdAt and updatedAt
+    public Item() {
+        this.task = "";
+        this.createdAt = getCurrentDate();
+        this.updatedAt = getCurrentDate();
+    }
+
+    // Get current time for updatedAt and createdAt
+    public String getCurrentDate() {
+        Date currDate = new Date();
+        DateFormat df = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS.SSS");
+
+        return df.format(currDate);
+    }
 }
