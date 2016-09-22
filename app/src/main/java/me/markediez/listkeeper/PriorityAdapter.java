0d6c0b810c778d9fe90546677dc516b1c1f14f65
Mark Diez <msdiez@ucdavis.edu>
2016-09-22 11:58:23 -0700

    Programitacally change the color of the tag located to the left of spinner choices

--- app/src/main/java/me/markediez/listkeeper/PriorityAdapter.java
+++ app/src/main/java/me/markediez/listkeeper/PriorityAdapter.java
@@ -1,10 +1,19 @@
 package me.markediez.listkeeper;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,6 +23,8 @@ import java.util.List;
  */
 
 public class PriorityAdapter extends ArrayAdapter<String> {
+    private ShapeDrawable tag;
+
     public PriorityAdapter(Context context, List<String> priorities) {
         super(context, 0, priorities);
     }
@@ -29,12 +40,25 @@ public class PriorityAdapter extends ArrayAdapter<String> {
 
     private View initView(int pos, View convertView, ViewGroup parent) {
         String str = getItem(pos);
+
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_priority, parent, false);
         }
 
         TextView tvPriority = (TextView) convertView.findViewById(R.id.tvPriority);
         tvPriority.setText(str);
+
+        ImageView ivTag = (ImageView)convertView.findViewById(R.id.ivTag);
+        GradientDrawable gdTag = (GradientDrawable) ivTag.getDrawable();
+
+        // TODO: Clean up. Maybe get the list of colors in an array?
+        if (str.equalsIgnoreCase("Low Priority")) {
+            gdTag.setColor(ContextCompat.getColor(convertView.getContext(), R.color.lowPriority));
+        } else if (str.equalsIgnoreCase("Normal Priority")) {
+            gdTag.setColor(ContextCompat.getColor(convertView.getContext(), R.color.normalPriority));
+        } else {
+            gdTag.setColor(ContextCompat.getColor(convertView.getContext(), R.color.highPriority));
+        }
 
         return convertView;
     }
